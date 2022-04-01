665044e998895a6098ae64b82bd151c60f629599
1Gize <grzolko@gmail.com>
2022-04-01 12:04:07 +0200

    created first test methods

--- src/test/scala/LetterValueSumSpec.scala
+++ src/test/scala/LetterValueSumSpec.scala
@@ -2,8 +2,14 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class LetterValueSumSpec extends AnyFlatSpec with should.Matchers{
-  def letterValueSum(s:String)= 1
+  def letterValueSum(s:String)= s match {
+    case "a" => 1
+    case "z" =>26
+  }
   it should "return value 1 for input a" in {
     letterValueSum("a") shouldBe 1
+  }
+  it should "return value 26 for input z" in {
+    letterValueSum("z") shouldBe 26
   }
 }
